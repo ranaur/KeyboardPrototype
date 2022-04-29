533b20105dcc353a5f38936e4ea0a358ae569ef4
Ranaur <ranaur@c3po.ranaur.net>
2022-04-29 00:28:43 -0300

    first version (Arduino UNO)

---
+++ KeyboardPrototype.ino
@@ -0,0 +1,107 @@
+typedef int8_t pin_t;
+typedef int state_t;
+
+unsigned long debounceDelay = 50;
+
+const pin_t columnsPins[] = { 8, 9, 10 };
+const int8_t nColumns = sizeof(columnsPins) / sizeof(pin_t);
+
+const pin_t rowsPins[] = { 4, 5, 6, 7 };
+const int8_t nRows = sizeof(rowsPins) / sizeof(pin_t);
+
+const char *rowColumnMappings[nRows][nColumns] = {
+  { "1", "2", "3" }, 
+  { "4", "5", "6" }, 
+  { "7", "8", "9" }, 
+  { "*", "0", "#" }
+};
+
+state_t rowColumnDebouncingState[nRows][nColumns];
+state_t rowColumnState[nRows][nColumns];
+unsigned long rowColumnDebouncingTime[nRows][nColumns];
+
+const char *rowColumnMap(int row, int column) {
+  return rowColumnMappings[row][column];
+}
+
+int8_t columnPin(int index) {
+  while(index < 0) index += nColumns;
+  return columnsPins[index % nColumns];
+}
+
+int8_t rowPin(int index) {
+  while(index < 0) index += nRows;
+  return rowsPins[index % nRows];
+}
+
+void setup() {
+  Serial.begin(9600);
+  
+  // Wait for USB Serial (Leonardo)
+  while (!Serial) {
+    yield();
+  }
+
+  Serial.println("Setup Begin!");
+
+  // Setup pins
+  for(int c = 0; c < nColumns; c++) {
+    pinMode(columnPin(c), OUTPUT);
+    digitalWrite(columnPin(c), HIGH);
+  }
+  for(int r = 0; r < nRows; r++) {
+    pinMode(rowPin(r), INPUT_PULLUP);
+  }
+
+  // Initialize state
+  for(int c = 0; c < nColumns; c++) {
+    for(int r = 0; r < nRows; r++) {
+      rowColumnState[r][c] = HIGH;
+      rowColumnDebouncingState[r][c] = HIGH;
+      rowColumnDebouncingTime[r][c] = 0;
+    }
+  }
+  Serial.println("Setup Complete!");
+}
+
+void loop() {
+  //Serial.println("NEW LOOP");
+  for(int c = 0; c < nColumns; c++) {
+    digitalWrite(columnPin(c - 1), HIGH);
+    digitalWrite(columnPin(c), LOW);
+
+    for(int r = 0; r < nRows; r++) {
+      int reading = digitalRead(rowsPins[r]);
+      
+      // debouncing
+      if (reading != rowColumnDebouncingState[r][c]) {
+        // reset the debouncing timer
+        rowColumnDebouncingTime[r][c] = millis();
+      }
+
+      if ((millis() - rowColumnDebouncingTime[r][c]) > debounceDelay) {
+        if (reading != rowColumnState[r][c]) {
+          rowColumnState[r][c] = reading;
+          if(reading == LOW) {
+            rowColumnDown(r, c);
+          } else {
+            rowColumnUp(r, c);
+          }
+        }
+      }
+
+      rowColumnDebouncingState[r][c] = reading;
+      
+    }
+  }
+}
+
+void rowColumnDown(int row, int column) {
+  Serial.print("DOWN ");
+  Serial.println(rowColumnMap(row, column));
+}
+
+void rowColumnUp(int row, int column) {
+  Serial.print("UP ");
+  Serial.println(rowColumnMap(row, column));
+}
